51f8057c01a38d3f41caca7af50ff96e64ec255c
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-10-04 00:16:32 +0200

    improve the runtime

--- news.py
+++ news.py
@@ -1,11 +1,13 @@
 import feedparser
 from datetime import date,datetime
 import time
-def rss_reader(url,art):
+
+def rss_reader(url):
     try:
-        return(feedparser.parse(url)["entries"][0][art])
+        return feedparser.parse(url)["entries"][0]
     except:
-        return()
+        return
+
 def clean(list):
     stop = False
     word = ''
@@ -17,21 +19,23 @@ def clean(list):
         if str(w) == '>':
             stop = False
     return(word.replace("\n",""))
+
 def get():
-    liste = ["https://tagesschau.de/xml/rss2","https://www.transfermarkt.de/rss/news"]
-    title1 = []
-    title = []
-    description1 = []
-    description = []
+    news_feeds = ["https://tagesschau.de/xml/rss2","https://www.transfermarkt.de/rss/news"]
+    title1 = list()
+    title = list()
+    description1 = list()
+    description = list()
     heute = datetime.strptime(time.strftime("%d.%m.%Y %H:%M:%S"), "%d.%m.%Y %H:%M:%S")
-    for datei in liste:
-            try:
-                date = datetime.fromtimestamp(time.mktime(rss_reader(datei, 'published_parsed')))
-                if (round((heute - date).seconds/3600, 0) <= 2):
-                    title1 += [rss_reader(datei, 'title')]
-                    description1 += [rss_reader(datei, 'description')]
-            except:
-                None
+    for news_feed in news_feeds:
+        try:
+            response = rss_reader(news_feed)
+            date = datetime.fromtimestamp(time.mktime(response['published_parsed']))
+            if (round((heute - date).seconds/3600, 0) <= 2):
+                title1 += [response['title']]
+                description1 += [response['description']]
+        except:
+            pass
     for i in description1:
         description += [clean(i)]
     for e in title1:
@@ -39,11 +43,11 @@ def get():
     if len(title) > 2:
         rss_str = ""
         for item in title:
-            rss_str = rss_str+str(item)+', <break time="100ms"/>'
-        return("{} und {}.".format(rss_str,title[-1]))
+            rss_str += str(item) + ', <break time="100ms"/>'
+        return "{} und {}.".format(rss_str, title[-1])
     elif len(title) == 1:
-        return("{}. {}".format(title[0],description[0]))
+        return "{}. {}".format(title[0], description[0])
     elif len(title) == 2:
-        return("{},<break time='100ms'/> {}".format(title[0],title[1]))
+        return "{},<break time='100ms'/> {}".format(title[0], title[1])
     else:
-        return("Es gibt keine News.")
+        return "Es gibt keine News."
